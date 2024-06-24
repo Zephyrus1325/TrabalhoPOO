3e9fff7fd0e22f46b5eb3781a23454322e6a4315
Bernardo <bernardofschirmer@gmail.com>
2024-06-24 18:02:41 -0300

    A adicionar_artigo está incompleta, vou tentar fazer ela quando chegar em casa Olhar também a de pesquisa, especificamente a database

--- src/chroma_search.py
+++ src/chroma_search.py
@@ -1,44 +1,44 @@
 import chromadb
+from artigo import Artigo
 
+#Criar ou adicionar um novo banco de dados
+def adicionar_artigo (artigo):
+    chclient = chromadb.PersistentClient(path = "database")
+    cpf_usuario = artigo.cpf_user
+    colecao_user = chclient.get_or_create_collection(name= cpf_usuario)
 
+    titulo = str(artigo.title)
+    resumo = str(artigo.summary)
 
-def pesquisa(keyword, quantidade):
-
-    chclient = chromadb.Client()    # Inicialização
-    collection = chclient.get_or_create_collection(name="my_collection") # Coleção para guardar os embeddings
-
-    # Mapear IDs para Docs
-    database = {
-        "Assembly": "This is a document about assembly",
-        "Python": "This is a document about python",
-        "Java": "This is a document about java"
-    }
-
-    # Atualizar a coleção com os documentos da database
-    collection.upsert(
-        documents=list(database.values()), # Lista de Todos os conteudos
-        ids=list(database.keys()) # Lista de IDs
+    colecao_user.add(
+        documents=[resumo], #Descricao dos resumos
+        metadatas=[{ #Contem o que vai ser mandado para a database
+            "title": titulo,
+            "resume": resumo,
+        }],
+        ids=[artigo.identifier] #ID da colecao
     )
 
+#Pegar o banco de dados e fazer uma pesquisa nele
+def pesquisa(keyword, quantidade, cpf_user):
+
+    chclient = chromadb.PersistentClient(path = "database")    # Inicialização
+    collection = chclient.get_or_create_collection(name= cpf_user) # Coleção para guardar os embeddings
+    
     # Input do User
     results = collection.query(
         query_texts=[str(keyword)],  # Prompt de pesquisa
-        n_results=quantidade,        # Quantos Resultados
+        n_results= int(quantidade),  # Quantos Resultados
     )
 
-    results.pop('metadatas')    # Remover os conteudos
+    results.pop('documents')    # Remover os conteudos
     results.pop('embeddings')   # que não retornam
     results.pop('uris')         # valores (None)
     results.pop('data')         #
 
-    # Criar lista de dicionarios
-    lista_final = []
+    # Criar lista de IDs
+    lista_id = []
     
-    for i in range(len(results['ids'][0])):
-        dicionario = {}
-        for chave, lista in results.items():
-            dicionario[chave] = lista[0][i]
-        lista_final.append(dicionario)
-    
-    return lista_final
-
+    for id in range(len(results['ids'][0])):
+        lista_id.append (results['ids'] [id])
+    return lista_id
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-# from chroma_search import pesquisa
 from flask import Flask
 from views import views
 import sqLite
+
+from artigo import Artigo
+import chroma_search as chr
 
 
 app = Flask(__name__)
@@ -9,4 +11,13 @@ app.register_blueprint(views, url_prefix="/")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    # app.run(debug=True, port=8000)
+
+    #EXEMPLO DE TESTE PARA O CHROMADB PARA CHECAR SE RETORNA OS IDS NA ORDEM 02 - 01
+    artigo1 = Artigo(ident= "01", title="Coding in Java", resume="Learn the basics of Java", link="https.Java.com", cpf_user="3456543", search_query="Java")
+    chr.adicionar_artigo(artigo1)
+
+    artigo1 = Artigo(ident= "02", title="Coding in Python", resume="Learn the basics of Python", link="https.python.com", cpf_user="3456543", search_query="Python")
+
+    chr.adicionar_artigo(artigo1)
+    chr.pesquisa("Python", "2", "3456543")
